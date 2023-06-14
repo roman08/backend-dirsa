fb296c0f091483599da8393083a32343b4cb2938
Roman Madrigal <rmcentinela@gmail.com>
2023-06-14 08:13:23 -0600

    update data base

--- database/migrations/2023_06_13_141609_update_name_v2_tables.php
+++ database/migrations/2023_06_13_141609_update_name_v2_tables.php
@@ -13,7 +13,7 @@ class UpdateNameV2Tables extends Migration
      */
     public function up()
     {
-        // Schema::rename('warehouse_entry', 'income_store'); //
+        Schema::rename('warehouse_entry', 'income_store'); //
         Schema::rename('warehouse_entry_detail', 'income_store_detail'); //
         Schema::rename('warehouse_income_type', 'catalog_income_type'); //
         Schema::rename('product_detail_warehouse_entry', 'product_income_store_detail'); //
